439b9041672459af0c9bd2178e02ec8fd3500598
Денис Барышев <deniska-baryshev@mail.ru>
2020-04-21 20:40:44 +0400

    change cat

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -8,7 +8,6 @@ using namespace std; // Искать имена в std.
 int main()
 {
     // Читаем из потока ввода символы и пишем их в поток вывода.
-    for (char ch; cin.get(ch);)
-        cout.put(ch);
+    cout << cin.rdbuf();
     return EXIT_SUCCESS; // Возвратим ОС "код успеха".
 }
