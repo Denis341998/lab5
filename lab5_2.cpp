694b9e7d1d5fd85b364d4788c171ea5617c26075
Денис Барышев <deniska-baryshev@mail.ru>
2020-04-21 20:27:33 +0400

    call func in_circle

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <clocale>
 
+using namespace std;
+
 // Определение своей функции-предиката.
 // проверить попадание в круг
 bool in_circle(float x, float y,
@@ -16,8 +18,13 @@ bool in_circle(float x, float y,
 
 int main()
 {
-  using namespace std;
-  setlocale(LC_ALL, "Russian");
-  cout << "Текст" << endl;
+  
+  cout << "Enter a sequence of coordinates x, y: ";
+  // Определение переменных; условие продолжения; последнее действие на каждом повторении.
+  for (float x, y; cin >> x >> y;)
+  {
+    const bool within_the_circle = in_circle(x, y, 1, -1, 3);
+    cout << "(x, y) within the circle == " << within_the_circle << endl;
+  }
   return EXIT_SUCCESS;
 }
