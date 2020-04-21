01019f95e5bcde027109860cef26cf11c1988b80
Денис Барышев <deniska-baryshev@mail.ru>
2020-04-21 20:26:31 +0400

    add func in_circle

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <clocale>
 
+// Определение своей функции-предиката.
+// проверить попадание в круг
+bool in_circle(float x, float y,
+  float cx, float cy, float r)
+  // координаты центра круга и его радиус
+{
+  // Константы -- после инициализации значения не изменяются.
+  const float dx = x - cx,
+              dy = y - cy;
+
+  return dx * dx + dy * dy <= r * r;
+}
+
 int main()
 {
   using namespace std;
