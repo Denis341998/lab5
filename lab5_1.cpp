25a35f2e3cb94f897830f16a3df76e3357f9ed1c
Денис Барышев <deniska-baryshev@mail.ru>
2020-04-21 19:56:25 +0400

    add strings

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <cstdlib>
+// Строки C++.
+#include <string>
 
 int main()
 {
-  std::cout << "Hello, user!";
+  using namespace std; // Искать имена в std.
+
+  string user_name = "user"; // Определить переменную.
+  cout << "Hello, " << user_name << "!" << endl;
+
+  user_name = "The Great Whale"; // Изменить значение переменной.
+  cout << "I am " << user_name;
   return EXIT_SUCCESS; // Возвратим ОС "код успеха".
 }
