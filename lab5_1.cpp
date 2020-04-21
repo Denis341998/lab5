14b334b9c0422e20aaca73721a2d80612c8bdfef
Денис Барышев <deniska-baryshev@mail.ru>
2020-04-21 20:31:42 +0400

    add for for

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -3,14 +3,18 @@
 // Строки C++.
 #include <string>
 
+using namespace std; // Искать имена в std.
+
 int main()
 {
-  using namespace std; // Искать имена в std.
-
-  string user_name = "user"; // Определить переменную.
-  cout << "Hello, " << user_name << "!" << endl;
-
-  user_name = "The Great Whale"; // Изменить значение переменной.
-  cout << "I am " << user_name;
+  
+// Определение переменных; условие продолжения; последнее действие на каждом повторении.
+  for (int i = 1; i <= 10; ++i) // ++i --> i = i + 1
+  {
+    for (int j = 1; j <= 10; ++j)
+    {
+      cout << i << "*" << j << "\t== " << i * j << endl;
+    }
+  }
   return EXIT_SUCCESS; // Возвратим ОС "код успеха".
 }
