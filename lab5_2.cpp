e98be7d2ea4f5cc94f4da9a44f23449ea96a85b1
Денис Барышев <deniska-baryshev@mail.ru>
2020-04-21 20:09:19 +0400

    add if

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <cstdlib>
+using namespace std;
 
 int main()
 {
-  std::cout << "Hello, user!";
-  return EXIT_SUCCESS; // Возвратим ОС "код успеха".
+  double x = 0;
+  cout << "x = ";
+  cin >> x;
+  cout << "x*x ";
+  if (x*x < 2) // Условие.
+    cout << " < ";
+  else // Альтернатива.
+    cout << " > ";
+  cout << "2" << endl;
+  return EXIT_SUCCESS;
 }
